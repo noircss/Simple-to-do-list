14b94376f70113b93c0b373676e76a72419e4c34
ShahRukh Ahmed <badshah21204@gmail.com>
2022-03-06 14:07:52 -0500

    1.3
    added enter event in javascript/jquery

--- docs/main.js
+++ docs/main.js
@@ -1,7 +1,30 @@
-let addToDoButton = document.getElementById('addToDo');
-let toDoContainer = document.getElementById('toDoContainer');
-let inputField = document.getElementById('inputField');
-
+const addToDoButton = document.getElementById('addToDo');
+const toDoContainer = document.getElementById('toDoContainer');
+const inputField = document.getElementById('inputField');
+document.addEventListener("DOMContentLoaded", function(e){
+    $(inputField).keyup(function yes(e){
+            if(e.key === 'Enter'){
+                var paragraph = document.createElement('p');
+                paragraph.classList.add('paragraph-styling');
+                paragraph.innerText = inputField.value;
+                toDoContainer.appendChild(paragraph);
+                inputField.value = "";
+                paragraph.addEventListener('click', function(e){
+                    paragraph.style.textDecoration = "line-through";
+                })
+                paragraph.addEventListener('mouseover', function(e){
+                    paragraph.style.textDecoration = "line-through";
+                })
+                paragraph.addEventListener('mouseout', function(e){
+                    paragraph.style.textDecoration = "none";
+                })
+                paragraph.addEventListener('dblclick', function(e){
+                    toDoContainer.removeChild(paragraph);
+                })
+                return;
+            }
+            
+        }); });
 addToDoButton.addEventListener('click', function(){
     var paragraph = document.createElement('p');
     paragraph.classList.add('paragraph-styling');
@@ -20,10 +43,5 @@ addToDoButton.addEventListener('click', function(){
     paragraph.addEventListener('dblclick', function(){
         toDoContainer.removeChild(paragraph);
     })
-    $(document).bind('keyup',function(){
-        $(inputField).keydown(function(e){ 
-            if(e.keyCode == 13 || e.which == 13){
-                $(addToDoButton).click();
-            }
-        }); });
+    
 })
